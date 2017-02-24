76240d5ce71ab1256102372b0ae04a93767f0d28
Guillaume Jenkins <gujenkin@microsoft.com>
2017-02-24 14:31:02 -0800

    Grid picker field

---
+++ core/field_dropdowngrid.js
@@ -0,0 +1,177 @@
+/**
+ * @license
+ * PXT Blockly
+ *
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * https://github.com/Microsoft/pxt-blockly
+ *
+ * See LICENSE file for details.
+ */
+/**
+ * @fileoverview Dropdown input field that arranges elements in a grid instead of a vertical list.
+ */
+/// <reference path="../localtypings/blockly.d.ts" />
+'use strict';
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+goog.provide('Blockly.FieldDropdownGrid');
+goog.require('Blockly.FieldDropdown');
+goog.require('goog.dom');
+goog.require('goog.events');
+goog.require('goog.positioning.ClientPosition');
+goog.require('goog.style');
+goog.require('goog.ui.Menu');
+goog.require('goog.ui.MenuItem');
+goog.require('goog.ui.Tooltip');
+var PxtFields;
+(function (PxtFields) {
+    var FieldDropdownGrid = (function (_super) {
+        __extends(FieldDropdownGrid, _super);
+        function FieldDropdownGrid(optionsValues, columns, useTooltips) {
+            if (columns === void 0) { columns = 1; }
+            if (useTooltips === void 0) { useTooltips = true; }
+            var _this = _super.call(this, optionsValues) || this;
+            _this.tooltips_ = [];
+            _this.columns_ = columns;
+            _this.useTooltips_ = useTooltips;
+            return _this;
+        }
+        FieldDropdownGrid.prototype.showEditor_ = function () {
+            Blockly.WidgetDiv.show(this, this.sourceBlock_.RTL, null);
+            var thisField = this;
+            this.disposeTooltips();
+            function callback(e) {
+                var menu = this;
+                var menuItem = e.target;
+                if (menuItem) {
+                    thisField.onItemSelected(menu, menuItem);
+                }
+                Blockly.WidgetDiv.hideIfOwner(thisField);
+                Blockly.Events.setGroup(false);
+                thisField.disposeTooltips();
+            }
+            var menu = new goog.ui.Menu();
+            menu.setRightToLeft(this.sourceBlock_.RTL);
+            var options = this.getOptions_();
+            for (var i = 0; i < options.length; i++) {
+                var content = options[i][0]; // Human-readable text or image.
+                var value = options[i][1]; // Language-neutral value.
+                if (typeof content == 'object') {
+                    // An image, not text.
+                    var image = new Image(content['width'], content['height']);
+                    image.src = content['src'];
+                    image.alt = content['alt'] || '';
+                    content = image;
+                }
+                var menuItem = new goog.ui.MenuItem(content);
+                menuItem.setRightToLeft(this.sourceBlock_.RTL);
+                menuItem.setValue(value);
+                menuItem.setCheckable(true);
+                menu.addChild(menuItem, true);
+                menuItem.setChecked(value == this.value_);
+            }
+            // Listen for mouse/keyboard events.
+            goog.events.listen(menu, goog.ui.Component.EventType.ACTION, callback);
+            // Listen for touch events (why doesn't Closure handle this already?).
+            function callbackTouchStart(e) {
+                var control = this.getOwnerControl(/** @type {Node} */ (e.target));
+                // Highlight the menu item.
+                control.handleMouseDown(e);
+            }
+            function callbackTouchEnd(e) {
+                var control = this.getOwnerControl(/** @type {Node} */ (e.target));
+                // Activate the menu item.
+                control.performActionInternal(e);
+            }
+            menu.getHandler().listen(menu.getElement(), goog.events.EventType.TOUCHSTART, callbackTouchStart);
+            menu.getHandler().listen(menu.getElement(), goog.events.EventType.TOUCHEND, callbackTouchEnd);
+            // Record windowSize and scrollOffset before adding menu.
+            var windowSize = goog.dom.getViewportSize();
+            var scrollOffset = goog.style.getViewportPageOffset(document);
+            var xy = this.getAbsoluteXY_();
+            var borderBBox = this.getScaledBBox_();
+            var div = Blockly.WidgetDiv.DIV;
+            menu.render(div);
+            var menuDom = menu.getElement();
+            Blockly.utils.addClass(menuDom, 'blocklyDropdownGridMenu');
+            // Record menuSize after adding menu.
+            var menuSize = goog.style.getSize(menuDom);
+            var menuItemsDom = menuDom.children;
+            var maxWidth = -1;
+            for (var i = 0; i < menuItemsDom.length; ++i) {
+                var currentSize = goog.style.getSize(menuItemsDom[i]);
+                if (currentSize.width > maxWidth) {
+                    maxWidth = currentSize.width;
+                }
+            }
+            menuSize.width = maxWidth * this.columns_;
+            goog.style.setWidth(menuDom, menuSize.width);
+            // Recalculate height for the total content, not only box height.
+            menuSize.height = menuDom.scrollHeight;
+            // Position the menu.
+            // Flip menu vertically if off the bottom.
+            if (xy.y + menuSize.height + borderBBox.height >=
+                windowSize.height + scrollOffset.y) {
+                xy.y -= menuSize.height + 2;
+            }
+            else {
+                xy.y += borderBBox.height;
+            }
+            if (this.sourceBlock_.RTL) {
+                xy.x += borderBBox.width;
+                xy.x += Blockly.FieldDropdown.CHECKMARK_OVERHANG;
+                // Don't go offscreen left.
+                if (xy.x < scrollOffset.x + menuSize.width) {
+                    xy.x = scrollOffset.x + menuSize.width;
+                }
+            }
+            else {
+                xy.x -= Blockly.FieldDropdown.CHECKMARK_OVERHANG;
+                // Don't go offscreen right.
+                if (xy.x > windowSize.width + scrollOffset.x - menuSize.width) {
+                    xy.x = windowSize.width + scrollOffset.x - menuSize.width;
+                }
+            }
+            Blockly.WidgetDiv.position(xy.x, xy.y, windowSize, scrollOffset, this.sourceBlock_.RTL);
+            if (this.useTooltips_) {
+                var _loop_1 = function (i) {
+                    var elem = menuItemsDom[i];
+                    var tooltip = new goog.ui.Tooltip(elem, options[i][0].alt || options[i][0]);
+                    var onShowOld = tooltip.onShow;
+                    tooltip.onShow = function () {
+                        onShowOld.call(tooltip);
+                        var newPos = new goog.positioning.ClientPosition(tooltip.cursorPosition.x + FieldDropdownGrid.TOOLTIP_X_OFFSET, tooltip.cursorPosition.y + FieldDropdownGrid.TOOLTIP_Y_OFFSET);
+                        tooltip.setPosition(newPos);
+                    };
+                    tooltip.setShowDelayMs(0);
+                    tooltip.className = 'goog-tooltip blocklyDropdownGridMenuItemTooltip';
+                    elem.addEventListener('mousemove', function (e) {
+                        var newPos = new goog.positioning.ClientPosition(e.clientX + FieldDropdownGrid.TOOLTIP_X_OFFSET, e.clientY + FieldDropdownGrid.TOOLTIP_Y_OFFSET);
+                        tooltip.setPosition(newPos);
+                    });
+                    this_1.tooltips_.push(tooltip);
+                };
+                var this_1 = this;
+                for (var i = 0; i < menuItemsDom.length; ++i) {
+                    _loop_1(i);
+                }
+            }
+            menu.setAllowAutoFocus(true);
+            menuDom.focus();
+        };
+        FieldDropdownGrid.prototype.disposeTooltips = function () {
+            if (this.tooltips_ && this.tooltips_.length) {
+                this.tooltips_.forEach(function (t) { return t.dispose(); });
+                this.tooltips_ = [];
+            }
+        };
+        return FieldDropdownGrid;
+    }(Blockly.FieldDropdown));
+    FieldDropdownGrid.TOOLTIP_X_OFFSET = 15;
+    FieldDropdownGrid.TOOLTIP_Y_OFFSET = -10;
+    PxtFields.FieldDropdownGrid = FieldDropdownGrid;
+})(PxtFields || (PxtFields = {}));
+Blockly.FieldDropdownGrid = PxtFields.FieldDropdownGrid;
---
+++ ts/core/field_dropdowngrid.ts
@@ -0,0 +1,200 @@
+/**
+ * @license
+ * PXT Blockly
+ *
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * https://github.com/Microsoft/pxt-blockly
+ * 
+ * See LICENSE file for details.
+ */
+
+/**
+ * @fileoverview Dropdown input field that arranges elements in a grid instead of a vertical list.
+ */
+
+/// <reference path="../localtypings/blockly.d.ts" />
+
+'use strict';
+
+goog.provide('Blockly.FieldDropdownGrid');
+
+goog.require('Blockly.FieldDropdown');
+goog.require('goog.dom');
+goog.require('goog.events');
+goog.require('goog.positioning.ClientPosition');
+goog.require('goog.style');
+goog.require('goog.ui.Menu');
+goog.require('goog.ui.MenuItem');
+goog.require('goog.ui.Tooltip');
+
+namespace PxtFields {
+    export class FieldDropdownGrid extends Blockly.FieldDropdown {
+        static TOOLTIP_X_OFFSET = 15;
+        static TOOLTIP_Y_OFFSET = -10;
+
+        private columns_: number;
+        private useTooltips_: boolean;
+        private tooltips_: goog.ui.Tooltip[] = [];
+
+        constructor(optionsValues: string[][], columns: number = 1, useTooltips: boolean = true) {
+            super(optionsValues);
+            this.columns_ = columns;
+            this.useTooltips_ = useTooltips;
+        }
+
+        public showEditor_() {
+            Blockly.WidgetDiv.show(this, this.sourceBlock_.RTL, null);
+            const thisField = this;
+
+            this.disposeTooltips();
+
+            function callback(e) {
+                const menu = this;
+                const menuItem = e.target;
+
+                if (menuItem) {
+                    thisField.onItemSelected(menu, menuItem);
+                }
+
+                Blockly.WidgetDiv.hideIfOwner(thisField);
+                Blockly.Events.setGroup(false);
+                thisField.disposeTooltips();
+            }
+
+            const menu = new goog.ui.Menu();
+            menu.setRightToLeft(this.sourceBlock_.RTL);
+            const options = this.getOptions_();
+
+            for (let i = 0; i < options.length; i++) {
+                let content = options[i][0]; // Human-readable text or image.
+                const value = options[i][1]; // Language-neutral value.
+
+                if (typeof content == 'object') {
+                    // An image, not text.
+                    const image = new Image(content['width'], content['height']);
+                    image.src = content['src'];
+                    image.alt = content['alt'] || '';
+                    content = image;
+                }
+
+                const menuItem = new goog.ui.MenuItem(content);
+                menuItem.setRightToLeft(this.sourceBlock_.RTL);
+                menuItem.setValue(value);
+                menuItem.setCheckable(true);
+                menu.addChild(menuItem, true);
+                menuItem.setChecked(value == this.value_);
+            }
+
+            // Listen for mouse/keyboard events.
+            goog.events.listen(menu, goog.ui.Component.EventType.ACTION, callback);
+
+            // Listen for touch events (why doesn't Closure handle this already?).
+            function callbackTouchStart(e) {
+                const control = this.getOwnerControl(/** @type {Node} */(e.target));
+                // Highlight the menu item.
+                control.handleMouseDown(e);
+            }
+
+            function callbackTouchEnd(e) {
+                const control = this.getOwnerControl(/** @type {Node} */(e.target));
+                // Activate the menu item.
+                control.performActionInternal(e);
+            }
+
+            menu.getHandler().listen(menu.getElement(), goog.events.EventType.TOUCHSTART,
+                callbackTouchStart);
+            menu.getHandler().listen(menu.getElement(), goog.events.EventType.TOUCHEND,
+                callbackTouchEnd);
+
+            // Record windowSize and scrollOffset before adding menu.
+            const windowSize = goog.dom.getViewportSize();
+            const scrollOffset = goog.style.getViewportPageOffset(document);
+            const xy = this.getAbsoluteXY_();
+            const borderBBox = this.getScaledBBox_();
+            const div = Blockly.WidgetDiv.DIV;
+            menu.render(div);
+            const menuDom = menu.getElement();
+            Blockly.utils.addClass(menuDom, 'blocklyDropdownGridMenu');
+
+            // Record menuSize after adding menu.
+            const menuSize = goog.style.getSize(menuDom);
+            const menuItemsDom = menuDom.children;
+            let maxWidth = -1;
+            for (let i = 0; i < menuItemsDom.length; ++i) {
+                const currentSize = goog.style.getSize(menuItemsDom[i]);
+
+                if (currentSize.width > maxWidth) {
+                    maxWidth = currentSize.width;
+                }
+            }
+
+            menuSize.width = maxWidth * this.columns_;
+            goog.style.setWidth(menuDom, menuSize.width);
+
+            // Recalculate height for the total content, not only box height.
+            menuSize.height = menuDom.scrollHeight;
+
+            // Position the menu.
+            // Flip menu vertically if off the bottom.
+            if (xy.y + menuSize.height + borderBBox.height >=
+                windowSize.height + scrollOffset.y) {
+                xy.y -= menuSize.height + 2;
+            } else {
+                xy.y += borderBBox.height;
+            }
+
+            if (this.sourceBlock_.RTL) {
+                xy.x += borderBBox.width;
+                xy.x += Blockly.FieldDropdown.CHECKMARK_OVERHANG;
+
+                // Don't go offscreen left.
+                if (xy.x < scrollOffset.x + menuSize.width) {
+                    xy.x = scrollOffset.x + menuSize.width;
+                }
+            } else {
+                xy.x -= Blockly.FieldDropdown.CHECKMARK_OVERHANG;
+
+                // Don't go offscreen right.
+                if (xy.x > windowSize.width + scrollOffset.x - menuSize.width) {
+                    xy.x = windowSize.width + scrollOffset.x - menuSize.width;
+                }
+            }
+            Blockly.WidgetDiv.position(xy.x, xy.y, windowSize, scrollOffset,
+                this.sourceBlock_.RTL);
+
+            if (this.useTooltips_) {
+                for (let i = 0; i < menuItemsDom.length; ++i) {
+                    const elem = menuItemsDom[i];
+                    const tooltip = new goog.ui.Tooltip(elem, options[i][0].alt || options[i][0]);
+                    const onShowOld = tooltip.onShow;
+                    tooltip.onShow = () => {
+                        onShowOld.call(tooltip);
+                        const newPos = new goog.positioning.ClientPosition(tooltip.cursorPosition.x + FieldDropdownGrid.TOOLTIP_X_OFFSET,
+                            tooltip.cursorPosition.y + FieldDropdownGrid.TOOLTIP_Y_OFFSET);
+                        tooltip.setPosition(newPos);
+                    };
+                    tooltip.setShowDelayMs(0);
+                    tooltip.className = 'goog-tooltip blocklyDropdownGridMenuItemTooltip';
+                    elem.addEventListener('mousemove', (e: MouseEvent) => {
+                        const newPos = new goog.positioning.ClientPosition(e.clientX + FieldDropdownGrid.TOOLTIP_X_OFFSET,
+                            e.clientY + FieldDropdownGrid.TOOLTIP_Y_OFFSET);
+                        tooltip.setPosition(newPos);
+                    });
+                    this.tooltips_.push(tooltip);
+                }
+            }
+
+            menu.setAllowAutoFocus(true);
+            (<any>menuDom).focus();
+        }
+
+        private disposeTooltips() {
+            if (this.tooltips_ && this.tooltips_.length) {
+                this.tooltips_.forEach((t) => t.dispose());
+                this.tooltips_ = [];
+            }
+        }
+    }
+}
+
+(Blockly as any).FieldDropdownGrid = PxtFields.FieldDropdownGrid;
