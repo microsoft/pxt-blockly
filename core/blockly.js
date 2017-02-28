a4eb35280f7b713d0b065740e9b94b38ad533eaf
Guillaume Jenkins <gujenkin@microsoft.com>
2017-02-27 17:21:01 -0800

    Adding missing require in blockly

--- core/blockly.js
+++ core/blockly.js
@@ -43,6 +43,7 @@ goog.require('Blockly.FieldColour');
 // Add it only if you need it.
 //goog.require('Blockly.FieldDate');
 goog.require('Blockly.FieldDropdown');
+goog.require('Blockly.FieldDropdownGrid');
 goog.require('Blockly.FieldImage');
 goog.require('Blockly.FieldTextInput');
 goog.require('Blockly.FieldNumber');
