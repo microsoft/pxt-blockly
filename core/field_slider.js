7e669a24ff7040aa72a13582ca703cb25db1afbc
Sam El-Husseini <samelh@microsoft.com>
2017-10-02 10:47:16 -0400

    Update field slider to expose readout.

--- core/field_slider.js
+++ core/field_slider.js
@@ -162,7 +162,7 @@ var pxtblocky;
             if (this.slider_ && this.readout_) {
                 // Update the slider background
                 this.setBackground_(this.slider_.getElement());
-                this.readout_.innerHTML = this.getValue();
+                this.setReadout_(this.readout_, this.getValue());
             }
         };
         ;
@@ -171,6 +171,9 @@ var pxtblocky;
                 goog.style.setStyle(slider, 'background', this.sliderColor_);
             else if (this.sourceBlock_.isShadow() && this.sourceBlock_.parentBlock_)
                 goog.style.setStyle(slider, 'background', this.sourceBlock_.parentBlock_.getColourTertiary());
+        };
+        FieldSlider.prototype.setReadout_ = function (readout, value) {
+            readout.innerHTML = value;
         };
         FieldSlider.prototype.updateSliderHandles_ = function () {
             if (this.slider_) {
--- ts/core/field_slider.ts
+++ ts/core/field_slider.ts
@@ -201,7 +201,7 @@ namespace pxtblocky {
             if (this.slider_ && this.readout_) {
                 // Update the slider background
                 this.setBackground_(this.slider_.getElement());
-                this.readout_.innerHTML = this.getValue();
+                this.setReadout_(this.readout_, this.getValue());
             }
         };
 
@@ -210,6 +210,10 @@ namespace pxtblocky {
                 goog.style.setStyle(slider, 'background', this.sliderColor_);
             else if (this.sourceBlock_.isShadow() && this.sourceBlock_.parentBlock_)
                 goog.style.setStyle(slider, 'background', this.sourceBlock_.parentBlock_.getColourTertiary());
+        }
+
+        setReadout_(readout: Element, value: string) {
+            readout.innerHTML = value;
         }
 
         updateSliderHandles_() {
