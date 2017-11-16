f74758bfcffa14df8bc0aa957f362b673e974181
Sam El-Husseini <samelh@microsoft.com>
2017-11-16 01:21:21 -0800

    Fix RTL heading icons

--- core/flyout_button.js
+++ core/flyout_button.js
@@ -186,7 +186,7 @@ Blockly.FlyoutButton.prototype.createDom = function() {
 
     svgIcon.setAttribute('text-anchor', 'end');
     svgIcon.setAttribute('alignment-baseline', 'central');
-    svgIcon.setAttribute('x', Blockly.FlyoutButton.MARGIN);
+    svgIcon.setAttribute('x', this.targetWorkspace_.RTL ? this.width : Blockly.FlyoutButton.MARGIN);
     svgIcon.setAttribute('y', this.height / 2);
   }
 
