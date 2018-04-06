65ccfb6b7e8c98578e7064a6f1a73d41c2f8f467
Sam El-Husseini <samelh@microsoft.com>
2018-04-06 15:26:33 -0700

    Minor refactor

--- core/workspace_svg.js
+++ core/workspace_svg.js
@@ -989,42 +989,43 @@ Blockly.WorkspaceSvg.prototype.centerOnBlock = function(id) {
     return;
   }
   var block = this.getBlockById(id);
-  if (block) {
-    // XY is in workspace coordinates.
-    var xy = block.getRelativeToSurfaceXY();
-    // Height/width is in workspace units.
-    var heightWidth = block.getHeightWidth();
-
-    // Center in workspace units.
-    var blockCenterX = xy.x + heightWidth.width / 2;
-    var blockCenterY = xy.y + heightWidth.height / 2;
-
-    // Workspace scale, used to convert from workspace coordinates to pixels.
-    var scale = this.scale;
-
-    // Center in pixels.  0, 0 is at the workspace origin.  These numbers may
-    // be negative.
-    var pixelX = blockCenterX * scale;
-    var pixelY = blockCenterY * scale;
-
-    var metrics = this.getMetrics();
-
-    // Scrolling to here would put the block in the top-left corner of the
-    // visible workspace.
-    var scrollToBlockX = pixelX - metrics.contentLeft;
-    var scrollToBlockY = pixelY - metrics.contentTop;
-
-    // viewHeight and viewWidth are in pixels.
-    var halfViewWidth = metrics.viewWidth / 2;
-    var halfViewHeight = metrics.viewHeight / 2;
-
-    // Put the block in the center of the visible workspace instead.
-    var scrollToCenterX = scrollToBlockX - halfViewWidth;
-    var scrollToCenterY = scrollToBlockY - halfViewHeight;
-
-    Blockly.hideChaff();
-    this.scrollbar.set(scrollToCenterX, scrollToCenterY);
+  if (!block) {
+    return;
   }
+  // XY is in workspace coordinates.
+  var xy = block.getRelativeToSurfaceXY();
+  // Height/width is in workspace units.
+  var heightWidth = block.getHeightWidth();
+
+  // Center in workspace units.
+  var blockCenterX = xy.x + heightWidth.width / 2;
+  var blockCenterY = xy.y + heightWidth.height / 2;
+
+  // Workspace scale, used to convert from workspace coordinates to pixels.
+  var scale = this.scale;
+
+  // Center in pixels.  0, 0 is at the workspace origin.  These numbers may
+  // be negative.
+  var pixelX = blockCenterX * scale;
+  var pixelY = blockCenterY * scale;
+
+  var metrics = this.getMetrics();
+
+  // Scrolling to here would put the block in the top-left corner of the
+  // visible workspace.
+  var scrollToBlockX = pixelX - metrics.contentLeft;
+  var scrollToBlockY = pixelY - metrics.contentTop;
+
+  // viewHeight and viewWidth are in pixels.
+  var halfViewWidth = metrics.viewWidth / 2;
+  var halfViewHeight = metrics.viewHeight / 2;
+
+  // Put the block in the center of the visible workspace instead.
+  var scrollToCenterX = scrollToBlockX - halfViewWidth;
+  var scrollToCenterY = scrollToBlockY - halfViewHeight;
+
+  Blockly.hideChaff();
+  this.scrollbar.set(scrollToCenterX, scrollToCenterY);
 };
 
 /**
