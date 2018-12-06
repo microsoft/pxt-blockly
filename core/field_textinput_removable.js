c0214ba64ca6efb399f1ea262a3d836819618010
Guillaume Jenkins <gujenkin@microsoft.com>
2018-12-06 18:38:15 -0500

    More fixes for MakeCode editor integration

--- core/field_textinput_removable.js
+++ core/field_textinput_removable.js
@@ -54,6 +54,13 @@ Blockly.FieldTextInputRemovable = function(text, opt_validator, opt_restrictor) 
 goog.inherits(Blockly.FieldTextInputRemovable, Blockly.FieldTextInput);
 
 /**
+ * Data URI for the delete argument icon.
+ * @type {string}
+ * @public
+ */
+Blockly.FieldTextInputRemovable.REMOVE_ARG_URI = "data:image/svg+xml;charset=UTF-8,%3c?xml version='1.0' encoding='UTF-8' standalone='no'?%3e%3csvg width='20px' height='20px' viewBox='0 0 20 20' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'%3e%3c!-- Generator: Sketch 48.1 (47250) - http://www.bohemiancoding.com/sketch --%3e%3ctitle%3edelete-argument v2%3c/title%3e%3cdesc%3eCreated with Sketch.%3c/desc%3e%3cdefs%3e%3c/defs%3e%3cg id='Page-1' stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'%3e%3cg id='delete-argument-v2' stroke='%23FF661A'%3e%3cg id='Group' transform='translate(3.000000, 2.500000)'%3e%3cpath d='M1,3 L13,3 L11.8900496,14.0995037 C11.8389294,14.6107055 11.4087639,15 10.8950124,15 L3.10498756,15 C2.59123611,15 2.16107055,14.6107055 2.10995037,14.0995037 L1,3 Z' id='Rectangle' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'%3e%3c/path%3e%3cpath d='M7,11 L7,6' id='Line' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'%3e%3c/path%3e%3cpath d='M9.5,11 L9.5,6' id='Line-Copy' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'%3e%3c/path%3e%3cpath d='M4.5,11 L4.5,6' id='Line-Copy-2' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'%3e%3c/path%3e%3crect id='Rectangle-2' fill='%23FF661A' x='0' y='2.5' width='14' height='1' rx='0.5'%3e%3c/rect%3e%3cpath d='M6,0 L8,0 C8.55228475,-1.01453063e-16 9,0.44771525 9,1 L9,3 L5,3 L5,1 C5,0.44771525 5.44771525,1.01453063e-16 6,0 Z' id='Rectangle-3' stroke-width='1.5'%3e%3c/path%3e%3c/g%3e%3c/g%3e%3c/g%3e%3c/svg%3e";
+
+/**
  * Show the inline free-text editor on top of the text with the remove button.
  * @private
  */
@@ -64,8 +71,7 @@ Blockly.FieldTextInputRemovable.prototype.showEditor_ = function() {
   div.className += ' removableTextInput';
   var removeButton =
       goog.dom.createDom(goog.dom.TagName.IMG, 'blocklyTextRemoveIcon');
-  removeButton.setAttribute('src',
-      Blockly.mainWorkspace.options.pathToMedia + 'removeArg.svg');
+  removeButton.setAttribute('src', Blockly.FieldTextInputRemovable.REMOVE_ARG_URI);
   this.removeButtonMouseWrapper_ = Blockly.bindEvent_(removeButton,
       'mousedown', this, this.removeCallback_);
   div.appendChild(removeButton);
