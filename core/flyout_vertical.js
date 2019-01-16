1f95c6ad0efaf2227ef5a51bef14bebbbaee14ba
Richard Knoll <riknoll@users.noreply.github.com>
2019-01-15 17:35:05 -0800

    Prevent flyout scrollbar from appearing off screen (#184)

--- core/flyout_vertical.js
+++ core/flyout_vertical.js
@@ -369,6 +369,14 @@ Blockly.VerticalFlyout.prototype.reflowInternal_ = function() {
   flyoutWidth *= this.workspace_.scale;
   flyoutWidth += Blockly.Scrollbar.scrollbarThickness;
 
+  // pxtblockly: restrict to the view width to prevent the
+  // scrollbar from rendering off-screen. This means if the workspace
+  // resizes, the flyout will be too small but that isn't an issue in pxt
+  var targetWorkspaceMetrics = this.targetWorkspace_.getMetrics();
+  if (targetWorkspaceMetrics) {
+    flyoutWidth = Math.min(flyoutWidth, targetWorkspaceMetrics.viewWidth);
+  }
+
   if (this.width_ != flyoutWidth) {
     for (var i = 0, block; block = blocks[i]; i++) {
       if (this.RTL) {
