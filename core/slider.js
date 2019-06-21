27486eed6e48db46588c5e81097c2d87071e255b
Chase Mortensen <C_Mortensen@live.com>
2019-06-21 16:49:29 -0700

    Added JSDoc comments

--- core/slider.js
+++ core/slider.js
@@ -15,32 +15,75 @@
 'use strict';
 
 goog.provide('Blockly.Slider');
-    
+
+/**
+ * Class for a slider.
+ * @constructor
+ */
 Blockly.Slider = function() {
     this.createDom();
     this.mouseDownWrapper = Blockly.bindEvent_(this.element_, "mousedown", this, this.onMouseDown);
 }
 
 /**
+ * The value held by the slider.
+ * @type {number}
+ * @private
+ */
+Blockly.Slider.prototype.value_ = 0;
+
+/**
+ * The minimum value set by the slider.
+ * @type {number}
+ * @private
+ */
+Blockly.Slider.prototype.min_ = 0;
+
+/**
+ * The maximum value set by the slider.
+ * @type {number}
+ * @private
+ */
+Blockly.Slider.prototype.max_ = 100;
+
+/**
  * Slider step (The smallest difference of values obtained using the slider).
+ * @type {number}
  * @private
  */
 Blockly.Slider.prototype.step_ = 1;
 
+/**
+ * Begins the mouse dragging
+ * @param {!Event} e Mouse move event.
+ */
 Blockly.Slider.prototype.onMouseDown = function(e) {
     this.updatePosition(e.clientX);
     this.mouseUpWrapper = Blockly.bindEvent_(window, "mouseup", this, this.onMouseUp);
     this.mouseMoveWrapper = Blockly.bindEvent_(window, "mousemove", this, this.onMouseMove);
 }
+
+/**
+ * Set the slider value to match the mouse's position.
+ * @param {!Event} e Mouse move event.
+ */
 Blockly.Slider.prototype.onMouseMove = function(e) {
     this.updatePosition(e.clientX);
 }
 
-Blockly.Slider.prototype.onMouseUp = function(e) {
+/**
+ * Ends the mouse dragging
+ * @param {!Event} e Mouse move event.
+ */
+Blockly.Slider.prototype.onMouseUp = function() {
     Blockly.unbindEvent_(this.mouseUpWrapper);
     Blockly.unbindEvent_(this.mouseMoveWrapper);
 }
 
+/**
+ * Updates the position of slider based on the given coordinate
+ * @param {!number} clientX The x coordinate of the mouse
+ */
 Blockly.Slider.prototype.updatePosition = function(clientX) {
     var rect = this.element_.getBoundingClientRect();
     var value = clientX - rect.left;
@@ -59,26 +102,44 @@ Blockly.Slider.prototype.updatePosition = function(clientX) {
     }
 }
 
+/**
+ * Sets whether or not the slider is visible
+ * @param {boolean} visible Whether or not the slider is visible
+ */
 Blockly.Slider.prototype.setVisible = function (visible) {
     this.getElement().style.display = visible ? '' : 'none';
 };
 
+/**
+ * Sets the minimum value obtained using the slider
+ * @param {number} min The minimum value
+ */
 Blockly.Slider.prototype.setMinimum = function (min) {
     this.min_ = min;
 };
 
+/**
+ * Sets the maximum value obtained using the slider
+ * @param {number} max The maximum value
+ */
 Blockly.Slider.prototype.setMaximum = function (max) {
     this.max_ = max;
 };
 
+/**
+ * Sets the step of the slider
+ * @param {number} step The step value for the slider
+ */
 Blockly.Slider.prototype.setStep = function (step) {
     this.step_ = step;
 };
 
-Blockly.Slider.prototype.setMoveToPointEnabled = function (enabled) {
-    this.moveToPoint = enabled;
-}
-
+/**
+ * Sets the value of the slider
+ * If the given value is greater than the max or less than the min, the slider 
+ * will be shown as clamped to that side, but the value of the slider will remain unchanged
+ * @param {!number} value The value for the slider
+ */
 Blockly.Slider.prototype.setValue = function (value) {
     this.value_ = value;
     value = Math.min(Math.max(value, this.min_), this.max_)
@@ -88,17 +149,30 @@ Blockly.Slider.prototype.setValue = function (value) {
     this.thumb_.style.left = value + "px";
 };
 
+/**
+ * Gets the current value of the slider
+ * If the given value is greater than the max or less than the min, the slider 
+ * will be shown as clamped to that side, but the value of the slider will remain unchanged
+ * @return {number} value The value for the slider
+ */
 Blockly.Slider.prototype.getValue = function () {
     return this.value_;
 };
 
-Blockly.Slider.prototype.render = function (parentElement, opt_beforeNode) {
+/**
+ * Renders the slider to the given element
+ * @param {!HTMLElement} the element the slider is to added to
+ */
+Blockly.Slider.prototype.render = function (parentElement) {
     if (!this.element_) {
         this.createDom();
     }
-    parentElement.insertBefore(this.element_, opt_beforeNode || null);
+    parentElement.appendChild(this.element_);
 };
 
+/**
+ * Create DOM elements for this slider.
+ */
 Blockly.Slider.prototype.createDom = function () {
     this.element_ = document.createElement('div');
     Blockly.utils.addClass(this.element_, 'slider');
@@ -111,6 +185,11 @@ Blockly.Slider.prototype.createDom = function () {
         this.onChangeEvent(this.value_);
     }
 };
+
+/**
+ * Gets the current element of the slider.
+ * @return {HTMLElement}
+ */
 Blockly.Slider.prototype.getElement = function () {
     return this.element_;
 };
