57f4c9a3c5a96e3bc0508bc6e81a9ac5ae0c2f6c
shakao <shakao@microsoft.com>
2020-04-07 16:28:08 -0700

    Only draw accessibility cursor if mode is enabled

--- core/block_svg.js
+++ core/block_svg.js
@@ -1845,7 +1845,7 @@ Blockly.BlockSvg.prototype.render = function(opt_bubble) {
   Blockly.utils.dom.stopTextWidthCache();
 
   var cursor = this.workspace.getCursor();
-  if (this.pathObject.cursorSvg_) {
+  if (this.workspace.keyboardAccessibilityMode && this.pathObject.cursorSvg_) {
     cursor.draw();
   }
 };
