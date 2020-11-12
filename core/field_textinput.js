e4472404c87c276b82ae640b230f36027244a4be
Vivian Li <vivl@microsoft.com>
2020-11-11 20:40:24 -0800

    Use isTouchEvent

--- core/field_textinput.js
+++ core/field_textinput.js
@@ -393,8 +393,8 @@ Blockly.FieldTextInput.prototype.showEditor_ = function(_opt_e,
 
   if (!quietInput && (Blockly.utils.userAgent.MOBILE ||
                       Blockly.utils.userAgent.ANDROID ||
-                      Blockly.utils.userAgent.IPAD) ||
-                      isTouchEvent) {
+                      Blockly.utils.userAgent.IPAD||
+                      isTouchEvent)) {
     this.showPromptEditor_();
   } else {
     this.showInlineEditor_(quietInput, readOnly, opt_withArrow, opt_arrowCallback);
